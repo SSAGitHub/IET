b32862cc41247c1826ed0e302c161163593ade98
Lukasz Lacki <llacki@gmail.com>
2018-04-06 15:08:33 +0200

    RSU-28 Applied code formatting

--- rsuite-plugin/src/main/java/org/theiet/rsuite/journals/domain/article/manuscript/acceptance/PublishOnAcceptance.java
+++ rsuite-plugin/src/main/java/org/theiet/rsuite/journals/domain/article/manuscript/acceptance/PublishOnAcceptance.java
@@ -19,22 +19,23 @@ import com.rsicms.projectshelper.utils.ProjectTransformationUtils;
 public class PublishOnAcceptance {
 
 	private static final String XSLT_URI_MANUSCRIPT_METADATA_TO_ARTICLE = "rsuite:/res/plugin/iet/xslt/journals/manuscript/manuscript-metadata-to-jats-article.xsl";
-	
+
 	private static final String XSLT_URI_MANUSCRIPT_RV_METADATA_TO_ARTICLE = "rsuite:/res/plugin/iet/xslt/journals/manuscript/manuscript-rv-metadata-to-jats-article.xsl";
 
 	private ExecutionContext context;
 
 	private User user;
-	
+
 	private Log logger;
-	
+
 	private String xsltTransformPath;
 
 	public PublishOnAcceptance(ExecutionContext context, User user, Log logger, ManifestType manifestType) {
 		this.context = context;
 		this.user = user;
 		this.logger = logger;
-		xsltTransformPath = manifestType == ManifestType.S1 ? XSLT_URI_MANUSCRIPT_METADATA_TO_ARTICLE :XSLT_URI_MANUSCRIPT_RV_METADATA_TO_ARTICLE;
+		xsltTransformPath = manifestType == ManifestType.S1 ? XSLT_URI_MANUSCRIPT_METADATA_TO_ARTICLE
+				: XSLT_URI_MANUSCRIPT_RV_METADATA_TO_ARTICLE;
 	}
 
 	public void publishOnAcceptance(Article article, ManuscriptPackage manuscriptPackage) throws RSuiteException {
@@ -46,29 +47,32 @@ public class PublishOnAcceptance {
 	private void sendToDigitalLibrary(Article article, File pdfFile, File articleFile) throws RSuiteException {
 		ArticleDigitalLibrary digitalLibrary = new ArticleDigitalLibrary(context, user, logger);
 		digitalLibrary.deliverArticleOnPublishAcceptance(article, pdfFile, articleFile);
-		
+
 	}
 
-	private File createInitialArticle(Article article, ManuscriptPackage manuscriptPackage, int numberOfPagesInPdf) throws RSuiteException {
+	private File createInitialArticle(Article article, ManuscriptPackage manuscriptPackage, int numberOfPagesInPdf)
+			throws RSuiteException {
 		Journal journal = article.getJournal();
-		
+
 		Map<String, String> parameters = new HashMap<>();
 		parameters.put("page-count", String.valueOf(numberOfPagesInPdf));
 		parameters.put("should-add-iet-prefix", String.valueOf(journal.requiresPrefixForDigitaLibrary()));
 		parameters.put("iet-prefix", journal.getPrefixForDigitaLibrary());
 		parameters.put("journal-abbrv-title", journal.getAbbreviatedTitle());
-		
+
 		String articleCode = article.getShortArticleId().replace("-", "");
 		File articleFile = new File(manuscriptPackage.getPackageFolder(), articleCode + ".xml");
-		ProjectTransformationUtils.transformDocument(context, manuscriptPackage.getMetadataFile(),
-				xsltTransformPath, articleFile, parameters);
+		ProjectTransformationUtils.transformDocument(context, manuscriptPackage.getMetadataFile(), xsltTransformPath,
+				articleFile, parameters);
 		return articleFile;
 	}
 
-	private InitialArticlePdf createInitialPDF(Article article, ManuscriptPackage manuscriptPackge) throws RSuiteException {
+	private InitialArticlePdf createInitialPDF(Article article, ManuscriptPackage manuscriptPackge)
+			throws RSuiteException {
 		File outputPdfFile = new File(manuscriptPackge.getPackageFolder(), article.getShortArticleId() + "-PROOF.pdf");
 		File scholarOnePdf = manuscriptPackge.getScholarOnePdf();
-		int numberOfPages = JournalCustomLibraryFactory.getInstance().createPdfForPublishOnAcceptance(scholarOnePdf, outputPdfFile);
+		int numberOfPages = JournalCustomLibraryFactory.getInstance().createPdfForPublishOnAcceptance(scholarOnePdf,
+				outputPdfFile);
 		return new InitialArticlePdf(outputPdfFile, numberOfPages);
 	}
 }
